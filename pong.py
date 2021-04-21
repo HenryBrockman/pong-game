987957c09052486b51f7ba1c130dc657873e5c25
HenryBrockman <hbrockman2024@kehillahstudent.org>
2021-04-21 15:34:25 -0700

    Initilize the pygame window and while loop

--- pong.py
+++ pong.py
@@ -1,2 +1,20 @@
 import pygame
 
+pygame.init()
+
+window_width = 750
+window_height = 600
+
+# set window and style
+win = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("PONG!")
+
+running = True
+while running:
+    pygame.time.delay(100)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            running = False
+
+pygame.quit()
