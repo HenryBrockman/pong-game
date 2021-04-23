fcbda2819848268b161d4a02224425b58f39024a
HenryBrockman <hbrockman2024@kehillahstudent.org>
2021-04-23 12:00:11 -0700

    Add player creation class (Broken)

--- pong.py
+++ pong.py
@@ -1,4 +1,4 @@
-
+# 16
 # Imports and Initilization
 import pygame
 from math import ceil, floor
@@ -13,11 +13,30 @@ class Ball:
         self.ballX = ballX
         self.ballY = ballY
         self.radius = radius
-        self.show()
+        self.build()
 
-    def show(self):
+    def build(self):
         pygame.draw.circle(self.win, self.color, (self.ballX, self.ballY), self.radius)
 
+class Player:
+    def __init__(self, win, color, playerX, playerY, rectWidth, rectHeight, vel):
+        self.win = win
+        self.color = color
+        self.playerX = playerX
+        self.playerY = playerY
+        self.rectWidth = rectWidth
+        self.rectHeight = rectHeight
+        self.vel = vel
+        self.build()
+        
+    def build(self):
+        pygame.draw.rect(self.win, self.color, (self.playerX, self.playerY, self.rectWidth, self.rectHeight))
+
+    def move_up(self):
+        self.playerY -= self.vel
+
+    def move_down(self):
+        self.playerY += self.vel
 
 # Code
 
@@ -46,12 +65,12 @@ p2_score = 0
 
 # build rectangle
 
-y1 = (ceil(WINDOW_HEIGHT/2)) - ceil(WINDOW_HEIGHT/6)
-y2 = (ceil(WINDOW_HEIGHT/2)) - ceil(WINDOW_HEIGHT/6)
+y1 = (WINDOW_HEIGHT//2) - (WINDOW_HEIGHT//6)/2
+y2 = (WINDOW_HEIGHT//2) - (WINDOW_HEIGHT//6)/2
 
 player_height = ceil(WINDOW_HEIGHT/6)
 
-# initilize movement booleans
+# initilize Booleans
 
 down1 = False
 up1 = False
@@ -59,9 +78,13 @@ up1 = False
 down2 = False
 up2 = False
 
-# Objects
+# Initilize Objects
 
 ball = Ball(win, white, WINDOW_WIDTH//2, WINDOW_HEIGHT//2, 14)
+
+player1 = Player(win, white, 20, y1, 10, player_height)
+player2 = Player(win, white, BASE - 35, y2, 10, player_height)
+
 
 running = True
 while running:
@@ -100,34 +123,55 @@ while running:
             if event.key == pygame.K_DOWN:
                 down2 = False
 
+    # if up1 and y1 > 0:
+    #     y1 -= VEL
+    #     win.fill(black)
+    # if down1 and y1 < WINDOW_HEIGHT - player_height:
+    #     y1 += VEL
+    #     win.fill(black)
+
+    # if up2 and y2 > 0:
+    #     y2 -= VEL
+    #     win.fill(black)
+    # if down2 and y2 < WINDOW_HEIGHT - player_height:
+    #     y2 += VEL
+    #     win.fill(black)
+
     if up1 and y1 > 0:
-        y1 -= VEL
-        win.fill(black)
+        player1.move_up()
     if down1 and y1 < WINDOW_HEIGHT - player_height:
-        y1 += VEL
-        win.fill(black)
+        player1.move_down()
 
     if up2 and y2 > 0:
-        y2 -= VEL
-        win.fill(black)
+        player2.move_up()
     if down2 and y2 < WINDOW_HEIGHT - player_height:
-        y2 += VEL
-        win.fill(black)
+        player2.move_down()
 
 
+    win.fill(black)
 
+    player1.build()
+    player2.build()
 
-    player1 = pygame.draw.rect(win, white, (20, y1, 10, player_height))
-    player2 = pygame.draw.rect(win, white, (BASE - 35, y2, 10, player_height))
-    
-    text = font.render("PONG", False, white, None)
-    win.blit(text, (BASE/2 - (text.get_rect().width / 2), 20))
+    ball.build()
 
     p1_score_text = font.render(str(p1_score), False, white, None)
     win.blit(p1_score_text, (ceil(BASE/15) - (p1_score_text.get_rect().width), 20))
 
     p2_score_text = font.render(str(p2_score), False, white, None)
     win.blit(p2_score_text, ((BASE - (ceil(BASE/15))) - (p2_score_text.get_rect().width), 20))
+
+    # player1 = pygame.draw.rect(win, white, (20, y1, 10, player_height))
+    # player2 = pygame.draw.rect(win, white, (BASE - 35, y2, 10, player_height))
+    
+    # text = font.render("PONG", False, white, None)
+    # win.blit(text, (BASE/2 - (text.get_rect().width / 2), 20))
+
+    # p1_score_text = font.render(str(p1_score), False, white, None)
+    # win.blit(p1_score_text, (ceil(BASE/15) - (p1_score_text.get_rect().width), 20))
+
+    # p2_score_text = font.render(str(p2_score), False, white, None)
+    # win.blit(p2_score_text, ((BASE - (ceil(BASE/15))) - (p2_score_text.get_rect().width), 20))
 
     pygame.display.update()
 
