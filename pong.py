1edc3822556421a8c4698c4c0291be380848132e
HenryBrockman <hbrockman2024@kehillahstudent.org>
2021-04-23 09:25:24 -0700

    Build two player sprites with y axis movement

--- pong.py
+++ pong.py
@@ -7,6 +7,8 @@ base = 750
 window_width = base
 window_height = int(base * 0.8)
 
+vel = 20
+
 # set window and style
 win = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("Pong!")
@@ -17,15 +19,23 @@ black = (0, 0, 0)
 white = (250, 250, 250)
 
 # build title
-font = pygame.font.SysFont('Bit5x3.tff', 32)
+font = pygame.font.SysFont('Bit5x5.tff', 32)
 
 p1_score = 0
 p2_score = 0
 
 # build rectangle
 
-player1 = pygame.draw.rect(win, white, (20, (ceil(window_height/2)) - ceil(window_height/6), 10, ceil(window_height/6)))
-player2 = pygame.draw.rect(win, white, (base - 20, (ceil(window_height/2)) - ceil(window_height/6), 10, ceil(window_height/6)))
+y1 = (ceil(window_height/2)) - ceil(window_height/6)
+y2 = (ceil(window_height/2)) - ceil(window_height/6)
+
+# initilize movement booleans
+
+down1 = False
+up1 = False
+
+down2 = False
+up2 = False
 
 running = True
 while running:
@@ -42,7 +52,47 @@ while running:
                 p2_score += 1
                 win.fill(black)
 
-    
+            if event.key == pygame.K_w:
+                up1 = True
+            if event.key == pygame.K_s:
+                down1 = True
+
+            if event.key == pygame.K_UP:
+                up2 = True
+            if event.key == pygame.K_DOWN:
+                down2 = True
+
+        if event.type == pygame.KEYUP:
+
+            if event.key == pygame.K_w:
+                up1 = False
+            if event.key == pygame.K_s:
+                down1 = False
+
+            if event.key == pygame.K_UP:
+                up2 = False
+            if event.key == pygame.K_DOWN:
+                down2 = False
+
+    if up1:
+        y1 -= vel
+        win.fill(black)
+    if down1:
+        y1 += vel
+        win.fill(black)
+
+    if up2:
+        y2 -= vel
+        win.fill(black)
+    if down2:
+        y2 += vel
+        win.fill(black)
+
+
+
+
+    player1 = pygame.draw.rect(win, white, (20, y1, 10, ceil(window_height/6)))
+    player2 = pygame.draw.rect(win, white, (base - 35, y2, 10, ceil(window_height/6)))
     
     text = font.render("PONG", False, white, None)
     win.blit(text, (window_width/2 - (text.get_rect().width / 2), 20))
